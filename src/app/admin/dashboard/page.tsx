a4345c801b343bd934db7fc4e4e2b98d69faac42
Manish Sonawane <manishsonawane3010@gmail.com>
2025-09-22 14:42:13 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { AdminDashboard } from "@/components/admin/dashboard/admin-dashboard";
+import { AdminDashboard } from "@/components/admin/admin-dashboard";
 
 export default function AdminDashboardPage() {
     return <AdminDashboard />;
