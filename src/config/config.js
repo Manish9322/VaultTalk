91b070ea8450c15d2c50cb2303c0c3e375590916
Manish Sonawane <manishsonawane3010@gmail.com>
2025-09-25 06:52:35 +0000

    create api.js file in services folder, then create config.js file in con

---
+++ src/config/config.js
@@ -0,0 +1,1 @@
+// This file will be used to configure and export environment variables for the application.
---
+++ src/services/api.js
@@ -0,0 +1,1 @@
+// This file will contain API-related logic, such as RTK Query endpoints.
